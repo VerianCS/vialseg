386e8776d9b20907d4bd9d060efca5759ae4844e
VerianCS <zafkiel023@gmail.com>
2024-05-21 10:39:19 -0400

    Finished defining the footer

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
 <div className="overflow-x-hidden">
   <div class="bottom-0 w-screen bg-slate-600">
       
-    <div class="mt-5 text-center text-1xl text-white font-Poppins">Made by Quartz Tiara Development</div>
+    <div class="mt-5 text-center text-1xl text-white font-Poppins">2024©</div>
   </div>
 </div>
 </Footer>
