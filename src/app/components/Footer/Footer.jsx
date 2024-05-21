c0f3bfa6c0c7b93a4d864e77bb48c2f1f052fc27
VerianCS <zafkiel023@gmail.com>
2024-05-21 07:21:35 -0400

    A little fix to the footer (:

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -4,12 +4,15 @@ export default function Footer() {
 
     return (
         <>
+<Footer>
 <div className="overflow-x-hidden">
   <div class="bottom-0 w-screen bg-slate-600">
-
+      
     <div class="mt-5 text-center text-1xl text-white font-Poppins">Made by Quartz Tiara Development</div>
   </div>
 </div>
+</Footer>
+
         </>
     )
 }
